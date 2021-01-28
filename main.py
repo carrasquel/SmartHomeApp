14809d2ca614247bda793d6b3f65f1991bd51446
Nelson Carrasquel <carrasquel@outlook.com>
2021-01-28 16:16:14 -0300

    Main module added

---
+++ main.py
@@ -0,0 +1,13 @@
+import tags
+import api
+import web
+import state_machines
+
+from rackio import Rackio
+
+if __name__ == '__main__':
+
+    app = Rackio()
+    app.set_log(file="app.log")
+
+    app.run()
